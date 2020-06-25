8d62e1bac84aad064cc2db70f657f39c40a2044b
RcA <36256014+RcA2650@users.noreply.github.com>
2020-06-25 23:42:21 +0530

    jkhjk
    jgiyt

---
+++ CollaBro/JS/index.js
@@ -0,0 +1,11 @@
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 10) {
+      $('nav').addClass('trnsp');
+    } else {
+      $('nav').removeClass('trnsp');
+    }
+  });
+
+  
+
+ 
---
+++ CollaBro/README.md
@@ -0,0 +1,2 @@
+# CollaBro
+A website made for the purpose of online collaboration for doing various kind of projects.
